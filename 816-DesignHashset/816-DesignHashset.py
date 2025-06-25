5c50da03220b4d26ef2b15a57256b1cc8ad5bfd5
Giolii <160989051+Giolii@users.noreply.github.com>
2025-06-25 12:57:17 -0700

    Create 816-DesignHashset

---
+++ 816-DesignHashset/816-DesignHashset.py
@@ -0,0 +1,27 @@
+# Last updated: 6/25/2025, 12:57:16 PM
+class MyHashSet:
+
+    def __init__(self):
+        self.buckets = [[] for _ in range(100)]
+
+    def add(self, key: int) -> None:
+        bucket = self.buckets[key % 100]
+        if key not in bucket:
+            bucket.append(key)
+
+    def remove(self, key: int) -> None:
+        bucket = self.buckets[key % 100]
+        if key in bucket:
+            bucket.remove(key)
+
+    def contains(self, key: int) -> bool:
+        bucket = self.buckets[key % 100]
+        return key in bucket
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
