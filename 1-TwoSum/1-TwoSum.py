0594b9bcc989a1e3f2efad3b1d52821f89020c0b
Giolii <160989051+Giolii@users.noreply.github.com>
2025-06-26 11:34:22 -0700

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,12 @@
+# Last updated: 6/26/2025, 11:34:22 AM
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        
+        for i,val in enumerate(nums):
+            m = target - val
+            if m in map:
+                return [i,map[m]]
+            map[val] = i
+        return []
+            
