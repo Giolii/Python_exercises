0b2f0e4a1c921c53f98a9e50a322d4fbaaa8ec0b
Giolii <160989051+Giolii@users.noreply.github.com>
2025-07-03 13:15:25 -0700

    Create 95-UniqueBinarySearchTreesIi

---
+++ 95-UniqueBinarySearchTreesIi/95-UniqueBinarySearchTreesIi.py
@@ -0,0 +1,33 @@
+# Last updated: 7/3/2025, 1:15:24 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        @cache
+        def generate_trees(l,r):
+            return [None] if l > r else [
+                TreeNode(val,left,right) for val in range(l,r+1)
+                for left in generate_trees(l,val - 1) 
+                for right in generate_trees(val + 1,r)
+            ]
+        return generate_trees(1,n)
+        
+        
+        
+        
+        
+        
+#         @cache
+#         def generate_trees(l, r):
+#             return [None] if l > r else [
+#                 TreeNode(val, left, right)
+#                 for val in range(l, r + 1)
+#                 for left in generate_trees(l, val - 1)
+#                 for right in generate_trees(val + 1, r)
+#             ]
+        
+#         return generate_trees(1, n)
