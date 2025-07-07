20fd131fc2d9cbca2b072d7619c9cd1b9a883df0
Giolii <160989051+Giolii@users.noreply.github.com>
2025-07-07 16:14:52 -0700

    Create 279-PerfectSquares

---
+++ 279-PerfectSquares/279-PerfectSquares.py
@@ -0,0 +1,12 @@
+# Last updated: 7/7/2025, 4:14:52 PM
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float('inf')] * (n+1)
+        dp[0] = 0
+        count = 1
+        while count * count <= n:
+            sq = count * count
+            for i in range(sq,n+1):
+                dp[i] = min(dp[i-sq] +1,dp[i])
+            count += 1
+        return dp[n]
