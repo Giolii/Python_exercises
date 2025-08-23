31233ec5db3a2c83dc40df177f3ef50dbdc0ec16
Giolii <160989051+Giolii@users.noreply.github.com>
2025-08-23 13:57:59 -0700

    Create 334-IncreasingTripletSubsequence

---
+++ 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.py
@@ -0,0 +1,16 @@
+# Last updated: 8/23/2025, 1:57:58 PM
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+        first = math.inf
+        second = math.inf
+
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+        return False
