607dae0e6afc5a33357835e36ac2a0fc6f10f70e
Giolii <160989051+Giolii@users.noreply.github.com>
2025-09-04 14:32:29 -0700

    Update 1013-FibonacciNumber.py

--- 1013-FibonacciNumber/1013-FibonacciNumber.py
+++ 1013-FibonacciNumber/1013-FibonacciNumber.py
@@ -1,16 +1,1 @@
-# Last updated: 6/30/2025, 4:59:43 PM
-class Solution:
-    def fib(self, n: int) -> int:
-        cache = {}
 
-        def fib_rec(n):
-            if n < 2:
-                return n
-            
-            if n in cache:
-                return cache[n]
-            
-            result = self.fib(n-1) + self.fib(n-2)
-            cache[n] = result
-            return result
-        return fib_rec(n)
