2d42c5c84822f3f7d42b42cd53ce2d915967634f
Luigi Olianas <160989051+Giolii@users.noreply.github.com>
2025-09-15 08:58:51 -0700

    Update 1127-LastStoneWeight.py

--- 1127-LastStoneWeight/1127-LastStoneWeight.py
+++ 1127-LastStoneWeight/1127-LastStoneWeight.py
@@ -1,9 +1,1 @@
-# Last updated: 7/21/2025, 3:19:56 PM
-class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
-        max_heap = [-stone for stone in stones]
-        heapq.heapify(max_heap)
 
-        while len(max_heap) > 1:
-            heapq.heappush(max_heap,heapq.heappop(max_heap) - heapq.heappop(max_heap))
-        return -max_heap[0]
