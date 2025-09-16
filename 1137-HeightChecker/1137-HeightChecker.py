a4c2066aaeb8cce6c27cded64ef4f8d20c9e1923
Luigi Olianas <160989051+Giolii@users.noreply.github.com>
2025-09-16 09:09:43 -0700

    Update 1137-HeightChecker.py

--- 1137-HeightChecker/1137-HeightChecker.py
+++ 1137-HeightChecker/1137-HeightChecker.py
@@ -1,1 +1,6 @@
 
+# Last updated: 6/25/2025, 11:42:55 AM
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        return sum(h != e for h, e in zip(heights, sorted(heights)))
+            
