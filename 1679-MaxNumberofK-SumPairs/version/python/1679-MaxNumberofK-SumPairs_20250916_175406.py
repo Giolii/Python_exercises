2f08f7b50f9ddef4527edec981ff84ffde7ec586
Luigi Olianas <160989051+Giolii@users.noreply.github.com>
2025-09-16 17:54:06 -0700

    Create 1679-MaxNumberofK-SumPairs

---
+++ 1679-MaxNumberofK-SumPairs/version/python/1679-MaxNumberofK-SumPairs_20250916_175406.py
@@ -0,0 +1,14 @@
+# Last updated: 9/16/2025, 5:54:06 PM
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        freq = Counter(nums)
+        count = 0
+        for key in freq:
+            if k - key in freq:
+                if k - key == key:
+                    count += freq[key] // 2
+                else:
+                    count += min(freq[key], freq[k - key])
+            freq[key] = 0
+        return count
+ 
