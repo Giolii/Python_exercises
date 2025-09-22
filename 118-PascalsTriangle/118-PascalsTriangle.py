d4373dd869c01924f40371f2279ae1f44217a200
Luigi Olianas <160989051+Giolii@users.noreply.github.com>
2025-09-22 10:13:43 -0700

    Update 118-PascalsTriangle.py

--- 118-PascalsTriangle/118-PascalsTriangle.py
+++ 118-PascalsTriangle/118-PascalsTriangle.py
@@ -1,18 +1,1 @@
-# Last updated: 6/25/2025, 11:43:34 AM
-class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 0:
-            return []
-        triangle = []
-        for i in range(numRows):
-            row = [1]
-            
-            for j in range(1,i):
-                row.append(triangle[i-1][j-1] + triangle[i-1][j])
-                
-            if i > 0:
-                row.append(1)
-                
-            triangle.append(row)
-        return triangle
-            
+
