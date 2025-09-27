921d7ac2a5e61dfd0cca2ea44fb88fb488ddcf3f
Luigi Olianas <160989051+Giolii@users.noreply.github.com>
2025-09-27 09:25:55 -0700

    Update 136-SingleNumber.py

--- 136-SingleNumber/136-SingleNumber.py
+++ 136-SingleNumber/136-SingleNumber.py
@@ -1,1 +1,10 @@
 
+# Last updated: 6/25/2025, 6:02:50 PM
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+    
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
+        
