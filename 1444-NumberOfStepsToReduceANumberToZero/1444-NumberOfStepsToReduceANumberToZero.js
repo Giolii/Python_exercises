ff37ee814026132767f3292425c9bb4e743a1b04
Luigi Olianas <160989051+Giolii@users.noreply.github.com>
2025-10-08 12:49:14 -0700

    Update 1444-NumberOfStepsToReduceANumberToZero.js

--- 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.js
+++ 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.js
@@ -1,16 +1,1 @@
-// Last updated: 6/25/2025, 11:42:48 AM
-/**
- * @param {number} num
- * @return {number}
- */
-
- function helper(num,steps){
-    if(num === 0) return steps
-    if(num%2 === 0) return helper(num/2,steps+1)
-    return helper(num-1,steps+1)
-}
-
-var numberOfSteps = function(num) {
-    return helper(num,0)
-};
 
